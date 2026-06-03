c69481907ccf65f20c3849765818f5082f0a700d
hari charan <kharicharan456@gmail.com>
2026-06-03 18:07:53 +0530

    fixed cors error

--- src/main/java/z/group/Zchat/Configurations/WebSocketConfig.java
+++ src/main/java/z/group/Zchat/Configurations/WebSocketConfig.java
@@ -20,7 +20,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/chat")
-                .setAllowedOrigins("http://localhost:5173")
+                .setAllowedOrigins("https://zchat-front-bj9ijedjf-charan0g0hs-projects.vercel.app")
                 .withSockJS();
     }
 
